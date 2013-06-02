79aacc1e5762d816af61ed93be0fbe9bc0df8d9a
md_5 <md_5@live.com.au>
2013-06-02 15:10:56 +1000

    Skeleton API Implementations

--- src/main/java/org/bukkit/craftbukkit/entity/CraftArrow.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftArrow.java
@@ -40,4 +40,15 @@ public class CraftArrow extends AbstractProjectile implements Arrow {
     public EntityType getType() {
         return EntityType.ARROW;
     }
+
+    // Spigot start
+    private final Arrow.Spigot spigot = new Arrow.Spigot()
+    {
+    };
+
+    public Arrow.Spigot spigot()
+    {
+        return spigot;
+    }
+    // Spigot end
 }
