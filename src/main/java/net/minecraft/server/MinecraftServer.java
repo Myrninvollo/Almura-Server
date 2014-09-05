pvginkel/Tweakkit-Server

Fully Disable Snooper When Not Required

src/main/java/net/minecraft/server/MinecraftServer.java
@@ -573,11 +573,11 @@ public abstract class MinecraftServer implements ICommandListener, Runnable, IMo
         this.f[this.ticks % 100] = System.nanoTime() - i;
         this.methodProfiler.b();
         this.methodProfiler.a("snooper");
-        if (!this.k.d() && this.ticks > 100) {
+        if (getSnooperEnabled() && !this.k.d() && this.ticks > 100) { // Spigot
             this.k.a();
         }
 
-        if (this.ticks % 6000 == 0) {
+        if (getSnooperEnabled() && this.ticks % 6000 == 0) { // Spigot
             this.k.b();
         }
 

Myrninvollo/Almura-Server

src/main/java/net/minecraft/server/MinecraftServer.java
@@ -498,11 +498,11 @@ public abstract class MinecraftServer implements ICommandListener, Runnable, IMo
         this.I = Packet.p;
         this.methodProfiler.b();
         this.methodProfiler.a("snooper");
-        if (!this.n.d() && this.ticks > 100) {
+        if (getSnooperEnabled() && !this.n.d() && this.ticks > 100) { // Spigot
             this.n.a();
         }
 
-        if (this.ticks % 6000 == 0) {
+        if (getSnooperEnabled() && this.ticks % 6000 == 0) { // Spigot
             this.n.b();
         }
 